0d75804579256c5dbda8157a2834d0cccf737841
Rosenberg-Contractor, Keith (CORP) <Keith.Rosenberg-Contractor@adp.com>
2017-04-20 08:25:14 -0400

    use document.ready function, add handler to .btn class and use data- attributes to determine which one was clicked, store DOM element references in topmost scope for reuse throughout the application

--- script.js
+++ script.js
@@ -1,61 +1,89 @@
+// Make sure you put your code in a document.ready!
+$(document).ready(function () {
+	// DOM element references
+	var $totalScore = $('#totalscore');
 
-
-//variables
-
-
-var totalScore = 0;
-
-
-
-
-
-
-//random number to start game 19-120
-
-
-var actualNumber = Math.floor(Math.random() * 101) + 19;
-
-console.log("Actual Number: " + actualNumber);
-
-
-//random number for each crystal  1-12
-
-var crystal1 = Math.floor(Math.random() * 12) + 1;
-var crystal2 = Math.floor(Math.random() * 12) + 1;
-var crystal3 = Math.floor(Math.random() * 12) + 1;
-var crystal4 = Math.floor(Math.random() * 12) + 1;
-
-console.log("crystal 1: " + crystal1);
-console.log("crystal 2: " + crystal2);
-console.log("crystal 3: " + crystal3);
-console.log("crystal 4: " + crystal4);
+	//variables
+	var totalScore = 0;
 
 
 
 
 
 
-//take that random number for each crystal and add it to a total
+	//random number to start game 19-120
+	var actualNumber = Math.floor(Math.random() * 101) + 19;
+	var totalScore = 0;
+
+	console.log("Actual Number: " + actualNumber);
 
 
-$("#greenBtn").on("click", function() {
-	var totalScore = ($(this).attr(crystal1));
-	totalScore = parseInt(totalScore);
-})
+	//random number for each crystal  1-12 
+	// Which crystal variable corresponds to which color?
+	// green = cystal1
+	var crystal1 = Math.floor(Math.random() * 12) + 1;
+	var crystal2 = Math.floor(Math.random() * 12) + 1;
+	var crystal3 = Math.floor(Math.random() * 12) + 1;
+	var crystal4 = Math.floor(Math.random() * 12) + 1;
 
-console.log("Compounding Score: " + totalScore)
-
-
-
-//display "win" if score matches first random game number
-
-
-
-//if player wins or loses then reset score
+	console.log("crystal 1: " + crystal1);
+	console.log("crystal 2: " + crystal2);
+	console.log("crystal 3: " + crystal3);
+	console.log("crystal 4: " + crystal4);
 
 
 
-//count number of wins and losses
 
+
+
+	//take that random number for each crystal and add it to a total
+	$(".btn").on("click", function() {
+		console.log("You clicked a gem! Which one is it?");
+		var gemColor = $(this).data('gem-color');
+
+		var scoreToAdd;
+
+		// TODO: add all the other scores!
+		switch (gemColor) {
+			case 'green':
+				scoreToAdd = crystal1;
+				break;
+			case 'yellow':
+				break;
+			case 'red':
+				break;
+			case 'blue':
+				break;
+			default:
+				console.log("There is no color " + gemColor + " - how did you get that?");
+				break;
+		}
+
+		totalScore += parseInt(scoreToAdd);
+		redraw();
+	})
+
+	console.log("Compounding Score: " + totalScore)
+
+	// redraw any HTML elements that might have been affected by interactions
+	function redraw () {
+		$totalScore.find('h1').text(totalScore);
+	}
+
+
+
+	//display "win" if score matches first random game number
+
+
+
+	//if player wins or loses then reset score
+
+
+
+	//count number of wins and losses
+
+	// Initially, draw the DOM so that total score is shown as 0
+	redraw();
+});
 
 
